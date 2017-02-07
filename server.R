270e0b2bbe1584c628c8815fda454c2d42f44fc4
Lucy <ld.mcgowan@vanderbilt.edu>
2017-02-07 14:48:57 -0600

    add sessionID and dropbox functionality

--- server.R
+++ server.R
@@ -2,31 +2,40 @@ library(shiny)
 library(shinythemes)
 library(lubridate)
 
+#add session_id
+session_id <- as.numeric(Sys.time())
+#make data upload to dropbox
+token <- readRDS("./ss-drop.rds")
+
 attendance = 0
 time.stamp = ymd_hms(Sys.time())
 time = minute(time.stamp) + second(time.stamp)/60
 start.time = paste0(hour(time.stamp),":",minute(time.stamp))
-
+tab.full = NULL
 shinyServer(
   function(input, output, session) {
-    
+    rv <- reactiveValues(
+      tab.full = data.frame(session_id   = session_id,
+                            time   = time,
+                            awake    = 0)
+    )
     observeEvent(input$increment, {
       attendance <<- c(attendance, tail(attendance,1)+1)
       
       time.stamp <<- ymd_hms(Sys.time())
       time <<- c(time, minute(time.stamp) + second(time.stamp)/60)
       
+      in.time <- (minute(time.stamp) + second(time.stamp)/60)
       max.participants <- max(attendance)
       current.participants <- tail(attendance,1)
       current.pct <- round((current.participants / max.participants)*100)
-      first.dropout <- ifelse(sum(diff(attendance) < 1) >0 , time[min(which(diff(attendance) < 0))], 0)
-      tab <- data.frame(rbind(max.participants,current.participants,current.pct,first.dropout), 
-                        row.names = c("Maximum N","Current N", "Current percent", "Time of first dropout"))
-      colnames(tab) <- " "
-      tab <<- tab
-      output$tab <- renderTable(tab)
+      first.dropout <- ifelse(sum(diff(attendance) < 1) >0 , time[min(which(diff(attendance) < 0),na.rm=TRUE)], 0)
+      tab <- data.frame(session_id = session_id, time = in.time, awake = 1)
+      rv$tab.full <<- rbind(rv$tab.full, tab)
+      output$tab <- renderTable(rv$tab.full[,-1])
     })
     
+
     observeEvent(input$decrement, {
       attendance <<- c(attendance, tail(attendance,1)-1)
       
@@ -37,24 +46,18 @@ shinyServer(
       time.stamp <<- ymd_hms(Sys.time())
       time <<- c(time, minute(time.stamp) + second(time.stamp)/60)
       
-      # output$timePlot <- renderPlot({
-      #   invalidateLater(2000, session)
-      #   plot(time, attendance, type = "s", xlim = c(min(time), max(time)+1))
-      # })
+      out.time <- (minute(time.stamp) + second(time.stamp)/60)
       
       max.participants <- max(attendance)
       current.participants <- tail(attendance,1)
       current.pct <- round((current.participants / max.participants)*100)
       first.dropout <- ifelse(sum(diff(attendance) < 1) >0 , time[min(which(diff(attendance) < 0))], 0)
-      tab <- data.frame(rbind(max.participants,current.participants,current.pct,first.dropout), 
-                        row.names = c("Maximum N","Current N", "Current percent", "Time of first dropout"))
-      colnames(tab) <- " "
-      tab <<- tab
-      output$tab <- renderTable(tab)
+      tab <- data.frame(session_id = session_id, time = out.time, awake = 0)
+      rv$tab.full <<- rbind(rv$tab.full, tab)
+      output$tab <- renderTable(rv$tab.full[,-1])
     })
     
-  
-    output$timePlot <- renderPlot({
+     output$timePlot <- renderPlot({
       invalidateLater(2000)
       attendance <<- c(attendance, tail(attendance,1))
       time.stamp <<- ymd_hms(Sys.time())
@@ -63,7 +66,11 @@ shinyServer(
       
     })
     
-
+     #output data
+     file_path <- file.path(tempdir(), paste0(round(session_id),".csv"))
+     readr::write_csv(isolate(rv$tab.full),file_path)    
+     #upload to dropbox 
+     rdrop2::drop_upload(file_path, "shiny/2016/seminarSurvival/", dtoken = token)
     
     
   }
